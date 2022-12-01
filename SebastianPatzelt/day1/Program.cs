fb17467ba3a539175416f0ac9fc31ce772b4fcde
Sebastian Patzelt <sebastian.patzelt@camao.one>
2022-12-01 12:25:18 +0100

    feat(day1): solution first part

--- SebastianPatzelt/day1/Program.cs
+++ SebastianPatzelt/day1/Program.cs
@@ -1,5 +1,16 @@
 using System;
 using System.Linq;
 using System.IO;
+using System.Collections.Generic;
 
-File.ReadLines("input.txt").ToList().ForEach(x=>Console.WriteLine(x));
+Console.WriteLine(
+    File.ReadLines("input.txt")
+        .Aggregate(new List<List<int>> { new List<int>() },
+        (list, value) =>
+        {
+            if (int.TryParse(value, out var number)) list.Last().Add(number);
+            else list.Add(new List<int>());
+            return list;
+        })
+        .Select(x => x.Sum())
+        .Max(x => x));
