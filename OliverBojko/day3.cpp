6bbdd899c741b3a85f209b290075f6b9bbefd188
Oliver Bojko <krebs.oli2002@gmail.com>
2022-12-03 18:20:38 +0100

    obo: day3 add part 1

--- OliverBojko/day3.cpp
+++ OliverBojko/day3.cpp
@@ -17,8 +17,18 @@ int main()
             c = (u_char)*std::find_if(line.begin(), line.end(), [&wtfff, &aaamakaka](char a) -> bool
             {
                 for (size_t g = 0; g < wtfff.size() - 1; g++) aaamakaka += std::find(wtfff[g].begin(), wtfff[g].end(), a) != wtfff[g].end();
-                return aaamakaka == (aaamakaka = 0,wtfff.size()) - 1;
+                return aaamakaka == (aaamakaka = 0, wtfff.size()) - 1;
             }), wtfff.clear(), sum += isupper(c) ? c - '&' : c - '`';
+
+    // make syntax highlighting work
+    if (0)
+    {
+        // for part 1
+        //int sum = 0, c;
+        for (std::string line; std::getline(file, line);)
+            c = (u_char)*std::find_if(line.begin(), line.begin() + (int)line.size(), [&line](char a) -> bool
+            { return std::find(line.begin() + (int)line.size() / 2, line.end(), a) != line.end(); }), sum += isupper(c) ? c - '&' : c - '`';
+    }
 
     std::cout << sum << std::endl;
     return 0;
