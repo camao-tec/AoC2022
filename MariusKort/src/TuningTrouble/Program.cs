8451cdf112580ad92c4be58c7c0a9646b0627627
Marius Kort <marius.kort@camao.one>
2022-12-11 20:44:25 +0100

    mak: day 6/7

---
+++ MariusKort/src/TuningTrouble/Program.cs
@@ -0,0 +1,21 @@
+﻿const byte PACKET_MARKER_LENGTH = 4;
+const byte MESSAGE_MARKER_LENGTH = 14;
+
+var input = await File.ReadAllTextAsync("input.txt");
+
+// part 1
+int packetMarkerIndex = GetMarkerIndex(input, PACKET_MARKER_LENGTH);
+Console.WriteLine($"start of packet after: {packetMarkerIndex}");
+
+// part 2
+var messsageMarkerIndex = GetMarkerIndex(input, MESSAGE_MARKER_LENGTH);
+Console.WriteLine($"start of message after: {messsageMarkerIndex}");
+
+static int GetMarkerIndex(string input, byte markerLength)
+{
+    var index = Enumerable.Range(0, input.Length - markerLength)
+        .TakeWhile(i => input.Skip(i).Take(markerLength).Distinct().Count() < markerLength)
+        .Last();
+
+    return index + markerLength + 1;
+}
